44a5eab988aa26d8a96d593db72073ec53dd2bfd
Ezra Lazuardy <ezralucio@gmail.com>
2021-07-22 09:04:53 +0700

    Update

--- src/helper/ExecutionTimeHelper.java
+++ src/helper/ExecutionTimeHelper.java
@@ -1,7 +1,22 @@
 package helper;
 
+/**
+ * <h3>ExecutionTimeHelper class</h3>
+ * Helper class to print the program execution time information
+ * <p>
+ * <h3>Author</h3>
+ * Ezra Lazuardy<br/>
+ * <a href="mailto:ezralucio@gmail.com">ezralucio@gmail.com</a><br/>
+ * <a href="https://ezralazuardy.com">https://ezralazuardy.com</a>
+ * </p>
+ */
 public class ExecutionTimeHelper {
 
+    /**
+     * Print the execution time in millisecond
+     *
+     * @param startTime long
+     */
     public static void printExecutionTime(long startTime) {
         System.out.format("%nExecution time: %d ms", (System.nanoTime() - startTime) / 1000000);
     }
--- src/helper/MemoryUsageHelper.java
+++ src/helper/MemoryUsageHelper.java
@@ -1,10 +1,28 @@
 package helper;
 
+/**
+ * <h3>MemoryUsageHelper class</h3>
+ * Helper class to print the program memory usage information
+ * <p>
+ * <h3>Author</h3>
+ * Ezra Lazuardy<br/>
+ * <a href="mailto:ezralucio@gmail.com">ezralucio@gmail.com</a><br/>
+ * <a href="https://ezralazuardy.com">https://ezralazuardy.com</a>
+ * </p>
+ */
 public class MemoryUsageHelper {
 
+    private static final Runtime runtime;
+
+    static {
+        runtime = Runtime.getRuntime();
+    }
+
+    /**
+     * Print the execution memory usage in Megabytes (MB)
+     */
     public static void printMemoryUsage() {
-        Runtime runtime = Runtime.getRuntime();
-        runtime.gc();
         System.out.format("%nMemory usage: %.1f MB%n", (double) (runtime.totalMemory() - runtime.freeMemory()) / (1024 * 1024));
+        runtime.gc();
     }
 }
