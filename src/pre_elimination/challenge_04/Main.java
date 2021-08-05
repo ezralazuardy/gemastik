85449ebbf2a19fc6ac38bcf4b741b02c27d64bfd
Ezra Lazuardy <ezralucio@gmail.com>
2021-08-05 19:02:36 +0700

    Update

--- src/pre_elimination/challenge_04/Main.java
+++ src/pre_elimination/challenge_04/Main.java
@@ -2,6 +2,137 @@ package pre_elimination.challenge_04;
 
 import java.util.Scanner;
 
+/**
+ * <h1>Menghitung Banyaknya Produk Unik</h1>
+ * <small>
+ * Time limit: 0.2 second / test case<br/>
+ * Memory limit: 64 MB
+ * </small>
+ * <br/><br/><hr/><br/>
+ * <h2>Deskripsi Masalah</h2>
+ * <p>
+ * Tahukah kamu siapa nama dari maskot GemasTIK XIII 2020? Nama dari maskot tersebut adalah
+ * Galih. Pada tahun 2120, ketika para harimau di Kerajaan Dayeuhkolot memiliki kemampuan
+ * membuat algoritma dan program, Galih, bersama dua saudaranya yang lain, yaitu Galuh dan
+ * Gilang, membuat sebuah perusahaan perangkat lunak yang dinamai PT DayeuhCoder.
+ * </p>
+ * <p>
+ * Setiap perangkat lunak yang dibuat oleh PT DayeuhCoder diberi nomor seri yang setiap digitnya
+ * merupakan bilangan bulat antara 0 hingga 9 (inklusif). Nomor seri ini merepresentasikan suatu
+ * bilangan bulat antara 0 hingga 1019 − 1 = 9 999 999 999 999 999 999 (inklusif, termasuk 0
+ * dan 1019 − 1.
+ * </p>
+ * <p>
+ * Galih, Galuh, dan Gilang memiliki kebiasaan yang tidak biasa pada perangkat lunak yang dibuat
+ * perusahaan mereka. Beberapa perangkat lunak yang dirilis untuk pengguna bisa jadi memiliki
+ * fitur tambahan yang unik. Pemberian fitur unik ini dijelaskan sebagai berikut. Galih, Galuh, dan
+ * Gilang memilih tiga bilangan berbeda, sebutlah 𝐴, 𝐵, dan 𝐶 dengan sifat 2 ≤ 𝐴,𝐵, 𝐶 ≤ 1019 −
+ * 1. Galih memberikan fitur tambahan unik pada produk yang nomor serinya habis dibagi 𝐴 saja
+ * (namun tidak habis dibagi oleh 𝐵 maupun 𝐶), Galuh memberikan fitur tambahan unik pada
+ * produk yang nomor serinya habis dibagi 𝐵 saja (namun tidak habis dibagi oleh 𝐴 maupun 𝐶),
+ * dan Gilang memberikan fitur tambahan unik pada produk yang nomor serinya habis dibagi oleh
+ * 𝐶 saja (namun tidak habis dibagi oleh 𝐴 maupun 𝐵).
+ * </p>
+ * <p>
+ * Suatu ketika, seekor harimau bernama Galang yang tinggal di kerajaan tetangga ingin
+ * mengetahui ada berapa banyak maksimal produk dengan fitur tambahan unik untuk sekumpulan
+ * perangkat lunak yang nomor serinya di antara 𝑀𝑖𝑛 dan 𝑀𝑎𝑥 (inklusif, termasuk 𝑀𝑖𝑛 dan 𝑀𝑎𝑥
+ * itu sendiri, dengan 𝑀𝑖𝑛 ≤ 𝑀𝑎𝑥) apabila dia juga mengetahui bilangan berbeda 𝐴, 𝐵, dan 𝐶
+ * yang dipilih oleh Galih, Galuh, dan Gilang. Tugas Anda adalah membantu Galang untuk
+ * menyelesaikan masalah ini.
+ * </p>
+ * <p>
+ * <h2>Format Masukan dan Keluaran</h2>
+ * Masukan terdiri dua baris, baris pertama adalah dua bilangan 𝑀𝑖𝑛 dan 𝑀𝑎𝑥 dengan 0 ≤ 𝑀𝑖𝑛 ≤
+ * 𝑀𝑎𝑥 ≤ 1019 − 1 yang dipisahkan dengan spasi. Baris kedua adalah tiga bilangan berbeda 𝐴,
+ * 𝐵, dan 𝐶 dengan 2 ≤ 𝐴,𝐵, 𝐶 ≤ 1019 − 1 dengan 𝐴 ≠ 𝐵 ≠ 𝐶 yang dipisahkan dengan spasi.
+ * </p>
+ * <p>
+ * Keluaran adalah banyaknya produk dengan fitur tambahan unik berdasarkan nomor serinya yang
+ * berada di antara 𝑀𝑖𝑛 dan 𝑀𝑎𝑥 (inklusif, termasuk 𝑀𝑖𝑛 dan 𝑀𝑎𝑥), sebagaimana dijelaskan pada
+ * deskripsi soal.
+ * </p>
+ * <p>
+ * <table style="width: 100%;">
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Masukkan 1</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * 1 20<br/>
+ * 9 4 6<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Keluaran 1</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * 6<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * </table>
+ * </p>
+ * <p>
+ * <table style="width: 100%;">
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Masukkan 2</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * 20 40<br/>
+ * 8 9 6<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Keluaran 2</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * 4<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * </table>
+ * </p>
+ * <p>
+ * <h2>Penjelasan Masukan/Keluaran</h2>
+ * Pada contoh masukan dan keluaran pertama, kita memiliki nilai 𝑀𝑖𝑛 = 1 dan 𝑀𝑎𝑥 = 20. Di
+ * antara dua bilangan ini, ada 6 bilangan yang merepresentasikan nomor seri produk dengan fitur
+ * tambahan unik sesuai dengan nilai 𝐴 = 9, 𝐵 = 4, dan 𝐶 = 6 sebagaimana kriteria pada soal,
+ * yaitu: 4, 6, 8, 9, 16, dan 20.
+ * </p>
+ * <p>
+ * Pada contoh masukan dan keluaran kedua, kita memiliki nilai 𝑀𝑖𝑛 =20 dan 𝑀𝑎𝑥 = 40. Di
+ * antara dua bilangan ini, ada 4 bilangan yang merepresentasikan nomor seri produk dengan fitur
+ * tambahan unik sesuai dengan nilai 𝐴 = 8, 𝐵 = 9, dan 𝐶 = 6 sebagaimana kriteria pada soal,
+ * yaitu: 27, 30, 32, dan 40.
+ * </p>
+ * <p>
+ * <h2>Solution</h2>
+ * Worst case time & space complexity:<code><b> ?</b></code>
+ * </p>
+ * <p>
+ * <h3>Author</h3>
+ * Ezra Lazuardy<br/>
+ * <a href="mailto:ezralucio@gmail.com">ezralucio@gmail.com</a><br/>
+ * <a href="https://ezralazuardy.com">https://ezralazuardy.com</a>
+ * </p>
+ */
+
 public class Main {
 
     /**
--- src/pre_elimination/challenge_05/Main.java
+++ src/pre_elimination/challenge_05/Main.java
@@ -5,76 +5,292 @@ import helper.MemoryUsageHelper;
 
 import java.util.Scanner;
 
+/**
+ * <h1>Dadu Multisisi</h1>
+ * <small>
+ * Time limit: 0.2 second / test case<br/>
+ * Memory limit: 16 MB
+ * </small>
+ * <br/><br/><hr/><br/>
+ * <h2>Deskripsi Masalah</h2>
+ * <p>
+ * Di Kerajaan Bojongsoang, Adhi dan Vijay adalah dua orang sahabat yang gemar bermain dadu.
+ * Alih-alih bermain dengan dadu konvensional yang memiliki enam sisi, mereka suka
+ * bereksperimen dengan dadu yang memiliki 𝑛 sisi dengan 3 ≤ 𝑛 ≤ 100. Setiap sisi dapat memuat
+ * label bilangan bulat 𝑚 dengan 1 ≤ 𝑚 ≤ 100. Angka yang terdapat pada sisi-sisi sebuah dadu
+ * dapat sama (baik pada dadu yang sama maupun dadu yang berbeda), namun Adhi dan Vijay
+ * menjamin bahwa peluang sebuah sisi muncul (menghadap ke atas) pada satu lemparan untuk dadu
+ * dengan 𝑛 sisi selalu sama, yaitu 1/𝑛. Sebagai contoh, peluang sebuah sisi muncul pada satu
+ * lemparan untuk sebuah dadu konvensional dengan enam sisi adalah 1/6. Beberapa bentuk dadu
+ * unik ini dapat dilihat pada Gambar 1.
+ * </p>
+ * <p>
+ * Mahen adalah teman baik dari Adhi dan Vijay. Suatu ketika ia ingin mengetahui siapa di antara
+ * mereka berdua yang memiliki peluang lebih besar dalam sebuah permainan dadu multisisi. Kedua
+ * pemain memainkan dua buah dadu dengan banyak sisi yang sama. Pemenang dari permainan
+ * adalah orang yang memiliki angka lebih besar daripada lawannya pada sisi dadu yang menghadap
+ * ke atas.
+ * </p>
+ * <p>
+ * Sebagai ilustrasi, misalkan Adhi dan Vijay bermain dadu dengan enam sisi. Dadu milik Adhi
+ * memiliki tiga buah angka 3 dan tiga buah angka 4, sedangkan dadu milik Vijay memiliki tiga buah
+ * angka 2 dan tiga buah angka 5. Adhi memenangkan permainan apabila dadunya memberikan
+ * angka yang lebih besar daripada dadu milik Vijay, dan sebaliknya. Contohnya, bila dadu milik
+ * Adhi memberikan angka 3 dan dadu milik Vijay menunjukkan angka 2, maka Adhi memenangkan
+ * permainan ini. Sedangkan apabila dadu milik Adhi memberikan angka 3 dan dadu milik Vijay
+ * menunjukkan angka 5, maka Vijay memenangkan permainan ini.
+ * </p>
+ * <p>
+ * Mahen ingin mengetahui siapakah di antara Adhi dan Vijay yang memiliki peluang yang lebih
+ * besar untuk memenangkan permainan dadu ini (atau mungkin saja keduanya memiliki peluang
+ * yang sama, dengan perkataan lain, permainan berakhir imbang). Pada contoh ilustrasi sebelumnya,
+ * kedua pemain memiliki peluang kemenangan yang sama (peluang kemenangan Adhi maupun
+ * Vijay masing-masing adalah 50%).
+ * </p>
+ * <p>
+ * <h2>Format Masukan dan Keluaran</h2>
+ * Masukan terdiri dari dua baris yang masing-masing memuat 𝑛 bilangan dengan 3 ≤ 𝑛 ≤ 100.
+ * Bilangan pada masing-masing baris merepresentasikan angka pada sebuah sisi dari dadu yang
+ * bersesuaian dan setiap bilangan berada pada rentang [1,100]. Informasi mengenai 𝑛 harus dibaca
+ * dari masukan. Bilangan-bilangan pada baris pertama merepresentasikan angka-angka yang
+ * terdapat pada sisi-sisi dadu milik Adhi, sedangkan bilangan-bilangan pada baris kedua menyatakan
+ * angka-angka yang terdapat pada sisi-sisi dadu miliki Vijay. Pada masing-masing dadu, peluang
+ * suatu sisi menghadap ke atas adalah sama (uniform), yaitu 1/𝑛 untuk dadu dengan 𝑛 sisi.
+ * </p>
+ * <p>
+ * Keluaran dapat berupa salah satu dari kondisi berikut:
+ * </p>
+ * <p>
+ * <ol>
+ * <li>String “Adhi menang dengan peluang x/y” tanpa tanda kutip apabila secara teori
+ * Adhi dapat memenangkan permainan dadu dengan peluang 𝑥/𝑦 dengan 0 ≤ 𝑥/𝑦 ≤ 1 dan
+ * 𝑥/𝑦 dinyatakan dalam pecahan paling sederhana.</li>
+ * <li>String “Vijay menang dengan peluang x/y” tanpa tanda kutip apabila secara
+ * teori Vijay dapat memenangkan permainan dadu dengan peluang 𝑥/𝑦 dengan 0 ≤ 𝑥/𝑦 ≤
+ * 1 dan 𝑥/𝑦 dinyatakan dalam pecahan paling sederhana.</li>
+ * <li>String “Imbang” tanpa tanda kutip apabila secara teori setiap pemain memiliki peluang
+ * 50% untuk memenangkan permainan dadu.</li>
+ * </ol>
+ * </p>
+ * <p>
+ * Catatan: pecahan paling sederhana adalah pecahan yang tidak dapat disederhanakan lagi.
+ * </p>
+ * <p>
+ * <table style="width: 100%;">
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Masukkan 1</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * 1 2 3 4 5 6<br/>
+ * 6 2 4 3 1 5<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Keluaran 1</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * Imbang<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * </table>
+ * </p>
+ * <p>
+ * <table style="width: 100%;">
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Masukkan 2</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * 3 3 4 4 3 4<br/>
+ * 2 5 5 5 2 2<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Keluaran 2</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * Imbang<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * </table>
+ * </p>
+ * <p>
+ * <table style="width: 100%;">
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Masukkan 3</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * 2 2 2 5 6 7<br/>
+ * 1 1 3 8 8 9<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Keluaran 3</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * Vijay menang dengan peluang 7/12<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * </table>
+ * </p>
+ * <p>
+ * <table style="width: 100%;">
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Masukkan 4</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * 3 5 7 9 8 1<br/>
+ * 3 4 1 2 1 4<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * <tr>
+ * <td style="text-align: center;">
+ * <h3>Keluaran 4</h3>
+ * </td>
+ * <td style="padding: 14px;">
+ * <code>
+ * Adhi menang dengan peluang 3/4<br/>
+ * </code>
+ * </td>
+ * </tr>
+ *
+ * </table>
+ * </p>
+ * <p>
+ * <h2>Penjelasan Masukan/Keluaran</h2>
+ * Pada contoh masukan dan keluaran pertama, dadu yang digunakan adalah dadu konvensional 6
+ * sisi yang setiap sisinya memiliki peluang 1/6 untuk menghadap ke atas. Dalam kasus ini baik
+ * Adhi maupun Vijay memiliki peluang yang sama untuk menang.
+ * </p>
+ * <p>
+ * Pada contoh masukan dan keluaran kedua, kita dapat menghitung bahwa baik Adhi maupun Vijay
+ * memiliki peluang kemenangan yang sama, yaitu 50%. Pada dadu milik Adhi, peluang masing-masing
+ * angka 3 atau angka 4 muncul adalah 1/2, sedangkan pada dadu milik Vijay, peluang
+ * masing-masing angka 2 atau 5 muncul adalah 1/2.
+ * </p>
+ * <p>
+ * Pada contoh masukan dan keluaran ketiga, pada dadu milik Adhi peluang angka 2 muncul adalah
+ * 3 ⋅ 1/6 = 1/2, sedangkan peluang masing-masing angka 5, 6, atau 7 muncul adalah 1/6. Pada
+ * dadu milik Vijay peluang masing-masing angka 1 atau 8 muncul adalah 2 ⋅ 1/6 = 1/3, sedangkan
+ * peluang masing-masing angka 3 atau 9 muncul adalah 1/6. Dengan formulasi statistika (yang
+ * harus Anda cari sendiri), kita dapat mengetahui bahwa Adhi berpeluang memenangkan permainan
+ * ini dengan peluang 7/12.
+ * </p>
+ * <p>
+ * <h2>Solution</h2>
+ * Worst case time & space complexity:<code><b> ?</b></code>
+ * </p>
+ * <p>
+ * <h3>Author</h3>
+ * Ezra Lazuardy<br/>
+ * <a href="mailto:ezralucio@gmail.com">ezralucio@gmail.com</a><br/>
+ * <a href="https://ezralazuardy.com">https://ezralazuardy.com</a>
+ * </p>
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-
         final Scanner in = new Scanner(System.in);
 
-        final String[] adhi = in.nextLine().trim().split(" ");
-        final String[] vijay = in.nextLine().trim().split(" ");
+        final String[] adhi = in.nextLine().split(" ");
+        final String[] vijay = in.nextLine().split(" ");
 
         in.close();
 
+        final long startTime = System.nanoTime();
+
+        if (adhi.length < 3 || adhi.length > 100 || vijay.length < 3 || vijay.length > 100)
+            throw new ArithmeticException("3-100 angka");
+
+        if (adhi.length != vijay.length)
+            throw new ArithmeticException("panjang tidak sama");
+
         int[] daduA = new int[adhi.length];
-        int maxA = Integer.MIN_VALUE;
         for (int i = 0; i < adhi.length; i++) {
             daduA[i] = Integer.parseInt(adhi[i]);
-            if (maxA < daduA[i]) maxA = daduA[i];
+            if (daduA[i] < 1 || daduA[i] > 100) throw new ArithmeticException("1-100 saja");
         }
 
         int[] daduB = new int[vijay.length];
-        int maxB = Integer.MIN_VALUE;
         for (int i = 0; i < vijay.length; i++) {
             daduB[i] = Integer.parseInt(vijay[i]);
-            if (maxB < daduB[i]) maxB = daduB[i];
+            if (daduB[i] < 1 || daduB[i] > 100) throw new ArithmeticException("1-100 saja");
         }
 
-        final long startTime = System.nanoTime();
-
-        int[] cntA = new int[maxA + 1];
-        int[] cntB = new int[maxB + 1];
+        // count peluang
+        int peluangA = 0, peluangB = 0;
         for (int i = 0; i < adhi.length; i++) {
-            cntA[daduA[i]]++;
-            cntB[daduB[i]]++;
-        }
-
-        // count probability
-        double[] probA = new double[maxA + 1];
-        double[] probB = new double[maxB + 1];
-        for (int i = 0; i < probA.length; i++) {
-            probA[i] = 1.0 * cntA[i] / adhi.length;
-        }
-        for (int i = 0; i < probB.length; i++) {
-            probB[i] = 1.0 * cntB[i] / vijay.length;
-        }
-
-        // count menang Adhi
-        double menangAdhi = 0.0;
-        for (int i = 0; i < probA.length; i++) {
-            for (int j = 0; j < probB.length; j++) {
-                if (probA[i] != 0 && probB[j] != 0 && i > j) {
-                    menangAdhi += probA[i] * probB[j];
-                }
+            for (int j = 0; j < vijay.length; j++) {
+                if (daduA[i] > daduB[j]) peluangA++;
+                if (daduB[i] > daduA[j]) peluangB++;
             }
         }
 
-        // count menang Vijay
-        double menangVijay = 0.0;
-        for (int i = 0; i < probB.length; i++) {
-            for (int j = 0; j < probA.length; j++) {
-                if (probB[i] != 0 && probA[j] != 0 && i > j) {
-                    menangVijay += probB[i] * probA[j];
-                }
-            }
-        }
-
-        if (menangVijay == menangAdhi) {
+        int bagi = 36;
+        if (peluangA == peluangB) {
             System.out.println("Imbang");
-        } else if (menangAdhi > menangVijay) {
-            System.out.format("Adhi menang dengan peluang %.2f%n", menangAdhi);
+        } else if (peluangA > peluangB) {
+            for (int i = 0; i < 1; ) {
+                if (peluangA % 2 == 0 && bagi % 2 == 0) {
+                    peluangA = peluangA / 2;
+                    bagi = bagi / 2;
+                } else if (peluangA % 3 == 0 && bagi % 3 == 0) {
+                    peluangA = peluangA / 3;
+                    bagi = bagi / 3;
+                } else if (peluangA % 5 == 0 && bagi % 5 == 0) {
+                    peluangA = peluangA / 5;
+                    bagi = bagi / 5;
+                } else i++;
+                if (i == 1)
+                    System.out.format("Adhi menang dengan peluang %d/%d%n", peluangA, bagi);
+            }
         } else {
-            System.out.format("Vijay menang dengan peluang %.2f%n", menangVijay);
+            for (int i = 0; i < 1; ) {
+                if (peluangB % 2 == 0 && bagi % 2 == 0) {
+                    peluangB = peluangB / 2;
+                    bagi = bagi / 2;
+                } else if (peluangB % 3 == 0 && bagi % 3 == 0) {
+                    peluangB = peluangB / 3;
+                    bagi = bagi / 3;
+                } else if (peluangB % 5 == 0 && bagi % 5 == 0) {
+                    peluangB = peluangB / 5;
+                    bagi = bagi / 5;
+                } else i++;
+                if (i == 1)
+                    System.out.format("Vijay menang dengan peluang %d/%d%n", peluangB, bagi);
+            }
         }
 
         ExecutionTimeHelper.printExecutionTime(startTime);
